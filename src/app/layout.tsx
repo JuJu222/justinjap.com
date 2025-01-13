ec29189129abad0e043fe2e364473383e12d7b8a
JuJu222 <japjustin@yahoo.com>
2025-01-13 19:42:33 +0700

    feat: added umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Navbar from './_components/navbar';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import Footer from './_components/footer';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -27,6 +28,12 @@ export default function RootLayout({
         <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
         <Footer />
       </body>
+      {/* Umami Analytics */}
+      <Script
+        defer
+        src='https://analytics.justinjap.com/script.js'
+        data-website-id='5614a698-1494-4d5b-b817-204376c62a22'
+      />
     </html>
   );
 }
