81158a789e5670cc2d7d0cc417993b67a5c7fcdc
JuJu222 <japjustin@yahoo.com>
2026-05-04 18:25:33 +0700

    chore: nextjs standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'd249xjcvkuj8xb.cloudfront.net',
-        pathname: '/justinjap.com/**',
+        protocol: "https",
+        hostname: "d249xjcvkuj8xb.cloudfront.net",
+        pathname: "/justinjap.com/**",
       },
     ],
   },
